c0ba6b1997eaf25a1fb1f5aa6eeab7e864138a77
Imalsha kalansooriya <30498171+kalan3124@users.noreply.github.com>
2024-09-13 14:37:08 +0530

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,8 +1,8 @@
 export const API_BASE_URL = "https://api.reimaginehome.ai";
-export const API_KEY = "66a78e94fd8bf883b1c9b432";
+export const API_KEY = "";
 export const SUPABASE_URL = "https://hlqksxtmdjdlptzebzkj.supabase.co";
 export const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhscWtzeHRtZGpkbHB0emViemtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MDc4NzMsImV4cCI6MjAzMjQ4Mzg3M30.LLJQ9UsI71SycLHkqEDsGhHfb__isGO1UEf0SfYZPco";
+  "";
 export const SUPABASE_BUCKET_PATH =
   "https://hlqksxtmdjdlptzebzkj.supabase.co/storage/v1/object/public/banigi-ai%20images/";
 
@@ -10,9 +10,9 @@ export const BACKEND_API_URL = "https://api.banigi.com";
 
 export const STRIP_URL = "https://buy.stripe.com/";
 export const STRIP_PUBLIC_KEY =
-  "pk_test_51PknplCFKIizQr1aNysXN2yEwPHiESmU10pBfBdocKlNVkYKucpCWGMrcOPTGD46WhkAERZJM3DjtO16ChQifila00HvHDkuPp";
+  "";
 export const STRIP_SECRET_KEY =
-  "sk_test_51PknplCFKIizQr1as5cs85QukfjSQiObdSEHpGd4n9llpWXU2HreGaFIZ3Zbqa9wM6mKHMA5G5JH2dtGK5XDtwaS00ceOIuGvI";
+  "";
 
 export const STRIP_PARTNER_PACK = "price_1PknswCFKIizQr1a8kkJQ3Dm";
 export const STRIP_BUSINESS_PACK = "price_1PknsdCFKIizQr1aPpYaqsAu";
